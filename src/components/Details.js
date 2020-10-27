cfa7645cb324787d98eebeb8ec63e3dc0860a83a
Nimsara Damith <nimsaradamith@Nimsaras-MacBook-Air.local>
2020-10-28 00:12:47 +0530

    second

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {ProductConsumer} from "../Context";
-import {ProductProvider} from "../Context";
 import {Link} from "react-router-dom";
 export  default  class Details extends Component {
     render() {
